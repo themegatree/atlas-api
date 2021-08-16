fe0e69b248922cd6363ca2a4eb1d1c9255d83a9f
unknown <benclayton1@hotmail.com>
2021-08-16 17:27:50 +0100

    added tables
    casing changed on files and file names

---
+++ models/modulechallenge.js
@@ -0,0 +1,25 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class ModuleChallenge extends Model {
+ 
+    static associate(models) {
+      this.Student = this.belongsTo(models.Student)
+    }
+  };
+  ModuleChallenge.init({
+    StudentId: DataTypes.INTEGER,
+    challengeName: DataTypes.STRING,
+    language: DataTypes.STRING,
+    studentScore: DataTypes.STRING,
+    coachScore: DataTypes.STRING,
+    dueDate: DataTypes.DATE,
+    submissionDate: DataTypes.DATE
+  }, {
+    sequelize,
+    modelName: 'ModuleChallenge',
+  });
+  return ModuleChallenge;
+};
--- models/student.js
+++ models/student.js
@@ -4,13 +4,9 @@ const {
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Student extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
+
     static associate(models) {
-      // define association here
+       this.ModuleChallenges = this.hasMany(models.ModuleChallenge, {onDelete: 'cascade'})
     }
   };
   Student.init({
