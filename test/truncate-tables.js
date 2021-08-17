63db07efeda7d3ec9c608c663b8368f050543ed9
dearshrewdwit <dearshrewdwit@gmail.com>
2021-08-17 09:39:13 +0100

    await truncation

--- test/truncate-tables.js
+++ test/truncate-tables.js
@@ -1,9 +1,9 @@
 const db = require('../models')
 
-const truncateTables = () => {
+const truncateTables = async () => {
   console.log('truncating tables')
-  db.Cohort.destroy({ truncate: true, cascade: true })
-  db.Student.destroy({ truncate: true, cascade: true })
+  await db.Cohort.destroy({ truncate: true, cascade: true })
+  await db.Student.destroy({ truncate: true, cascade: true })
 }
 
 module.exports = truncateTables
