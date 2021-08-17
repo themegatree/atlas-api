f36dd906cf459e2e9436cf2adbe425d759d15ce6
Bridget <bridgetshidok@gmail.com>
2021-08-17 19:22:52 +0100

    fixed quotation issue

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
-const express = require("express");
-const app = express();
-const port = 5000;
-const cors = require("cors");
+const express = require('express')
+const app = express()
+const port = 5000
+const cors = require('cors')
 
-const cohortsRouter = require("./routes/cohorts.js");
-const studentsRouter = require("./routes/students.js");
+const cohortsRouter = require('./routes/cohorts.js')
+const studentsRouter = require('./routes/students.js')
 
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
-app.use("/api/cohorts", cohortsRouter);
-app.use("/api/students", studentsRouter);
+app.use('/api/cohorts', cohortsRouter)
+app.use('/api/students', studentsRouter)
 
 app.listen(port, () => {
-  "listening on port: ${port}";
-});
+  "listening on port: ${port}"
+})
--- test/create-cohort.js
+++ test/create-cohort.js
@@ -1,10 +1,10 @@
-const { Cohort } = require("../models");
+const { Cohort } = require('../models');
 
 const createCohort = async () => {
-  console.log("creating cohort");
+  console.log('creating cohort');
   await Cohort.create({
-    name: "test cohort",
-    date: "12/01/2020",
+    name: 'test cohort',
+    date: '12/01/2020',
   });
 };
 
