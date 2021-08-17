cf74096486971a6fb17f22e6dc11778974e65d91
Colin Gardiner <50584283+FractalFraction@users.noreply.github.com>
2021-08-17 09:20:50 +0100

    AT-20 Update-remove comment student gender column
    Removed comments from the students gender column.

--- migrations/20210816164930-add-student-gender-column.js
+++ migrations/20210816164930-add-student-gender-column.js
@@ -2,26 +2,10 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
-
     await queryInterface.addColumn('Students','gender', Sequelize.STRING)
-
-
   },
 
   down: async (queryInterface, Sequelize) => {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
     await queryInterface.removeColumn('Students', 'gender')
-
   }
 };
