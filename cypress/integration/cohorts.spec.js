368742931d78da75976bfa59ba61992852762075
Bridget <bridgetshidok@gmail.com>
2021-08-17 19:48:55 +0100

    Revert "adjusted file to origin"
    This reverts commit 0d593b4e84936374420f25ac93374324e5dd26fa.

--- cypress/integration/cohorts.spec.js
+++ cypress/integration/cohorts.spec.js
@@ -1,16 +1,85 @@
-describe("Cohorts", function() {
-  it("returns cohorts", function() {
-    cy.task('taskTruncateTables')
-      .then(() => {
-        cy.task('taskCreateCohort')
+describe("Cohorts", function () {
+  it("returns cohorts to main api", function () {
+    cy.task("taskTruncateTables").then(() => {
+      cy.task("taskCreateCohort");
+      cy.request({
+        method: "GET",
+        url: "/api/cohorts",
+      }).should((res) => {
+        expect(res.body.cohorts.length).to.eq(1);
+        expect(res.body.cohorts[0].name).to.eq("test cohort");
+        expect(res.status).to.eq(200);
+      });
+    });
+  });
+
+    it("returns cohorts to sort by name", function () {
+      cy.task("taskTruncateTables").then(() => {
+        cy.task("taskCreateCohort");
         cy.request({
           method: "GET",
-          url: "api/cohorts"
-        }).should(res => {
-          expect(res.body.cohorts.length).to.eq(1)
-          expect(res.body.cohorts[0].name).to.eq("test cohort")
-          expect(res.status).to.eq(200)
-        })
-    })
-  })
-})
+          url: "/api/cohorts?sort=name",
+        }).should((res) => {
+          expect(res.body.cohorts.length).to.eq(1);
+          expect(res.body.cohorts[0].name).to.eq("test cohort");
+          expect(res.status).to.eq(200);
+        });
+      });
+    });
+
+    it("returns cohorts to sort by date", function () {
+      cy.task("taskTruncateTables").then(() => {
+        cy.task("taskCreateCohort");
+        cy.request({
+          method: "GET",
+          url: "/api/cohorts?sort=date",
+        }).should((res) => {
+          expect(res.body.cohorts.length).to.eq(1);
+          expect(res.body.cohorts[0].name).to.eq("test cohort");
+          expect(res.status).to.eq(200);
+        });
+      });
+    });
+
+    it("returns cohorts to sort in decending order", function () {
+      cy.task("taskTruncateTables").then(() => {
+        cy.task("taskCreateCohort");
+        cy.request({
+          method: "GET",
+          url: "/api/cohorts?order=descending",
+        }).should((res) => {
+          expect(res.body.cohorts.length).to.eq(1);
+          expect(res.body.cohorts[0].name).to.eq("test cohort");
+          expect(res.status).to.eq(200);
+        });
+      });
+    });
+
+    it("returns cohorts to sort in acending order", function () {
+      cy.task("taskTruncateTables").then(() => {
+        cy.task("taskCreateCohort");
+        cy.request({
+          method: "GET",
+          url: "/api/cohorts?order=ascending",
+        }).should((res) => {
+          expect(res.body.cohorts.length).to.eq(1);
+          expect(res.body.cohorts[0].name).to.eq("test cohort");
+          expect(res.status).to.eq(200);
+        });
+      });
+    });
+
+    it("returns cohorts to /api/cohorts?sort=date&order=descending", function () {
+      cy.task("taskTruncateTables").then(() => {
+        cy.task("taskCreateCohort");
+        cy.request({
+          method: "GET",
+          url: "/api/cohorts?sort=date&order=descending",
+        }).should((res) => {
+          expect(res.body.cohorts.length).to.eq(1);
+          expect(res.body.cohorts[0].name).to.eq("test cohort");
+          expect(res.status).to.eq(200);
+        });
+      });
+    });
+});
--- routes/cohorts.js
+++ routes/cohorts.js
@@ -3,10 +3,17 @@ const router = express.Router()
 
 const { Cohort } = require('../models')
 
+//Update API to handle request to `/api/cohorts?sort=date&order=descending`
 router.get('/', async function (req, res) {
-  const cohorts = await Cohort.findAll({ include: { all: true }})
-
+  const sort = req.params.sort
+  const order = req.params.order
+  const cohorts = await Cohort.findAll({
+    include: { all: true },
+    order: [[sort, order]],
+  })
+  //we use json to pass
   res.json({ cohorts: cohorts })
 })
+
 
 module.exports = router
