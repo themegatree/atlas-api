75d69cef2979a5b7bc8204676d2114448be92bce
Bridget <bridgetshidok@gmail.com>
2021-08-17 19:56:12 +0100

    fixed quotations

--- routes/cohorts.js
+++ routes/cohorts.js
@@ -1,19 +1,19 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
-const { Cohort } = require('../models')
+const { Cohort } = require("../models");
 
 //Update API to handle request to `/api/cohorts?sort=date&order=descending`
 router.get('/', async function (req, res) {
-  const sort = req.params.sort
-  const order = req.params.order
+  const sort = req.params.sort;
+  const order = req.params.order;
   const cohorts = await Cohort.findAll({
     include: { all: true },
     order: [[sort, order]],
-  })
+  });
   //we use json to pass
-  res.json({ cohorts: cohorts })
-})
+  res.json({ cohorts: cohorts });
+});
 
 
-module.exports = router
+module.exports = router;
