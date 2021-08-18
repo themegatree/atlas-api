b812bd884229e950fd5c2f08dd4a77986bd1a291
Ben Ferenczy <bferenczy@gmail.com>
2021-08-18 15:44:12 +0100

    awaited truncating tables

--- cypress/integration/students.spec.js
+++ cypress/integration/students.spec.js
@@ -1,20 +1,19 @@
-describe("Cohorts", function() {
-  beforeEach(function() {
-    cy.task('taskTruncateTables')
-  })
-  
+describe("Students", function() {
   it("returns students", function() {
-    cy.task('taskCreateStudent')
-    cy.request({
-      method: "GET",
-      url: "api/students"
-    }).should(res => {
-      expect(res.body.students.length).to.eq(4)
-      expect(res.body.students[0].firstName).to.eq("Alice")
-      expect(res.body.students[0].lastName).to.eq("Williams")
-      expect(res.body.students[0].githubUsername).to.eq("alicewilliamsgit")
-      expect(res.body.students[0].email).to.eq("awilliams@email.com")
-      expect(res.status).to.eq(200)
+    cy.task('taskTruncateTables')
+      .then(() => {
+        cy.task('taskCreateStudent')
+        cy.request({
+          method: "GET",
+          url: "api/students"
+        }).should(res => {
+          expect(res.body.students.length).to.eq(4)
+          expect(res.body.students[0].firstName).to.eq("Alice")
+          expect(res.body.students[0].lastName).to.eq("Williams")
+          expect(res.body.students[0].githubUsername).to.eq("alicewilliamsgit")
+          expect(res.body.students[0].email).to.eq("awilliams@email.com")
+          expect(res.status).to.eq(200)
+      })
     })
   })
 })
