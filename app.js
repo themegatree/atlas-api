6129cd7b4c96704fe6f1f7f5a9fa73fd60e2c3a3
OllieVanD <Ollievdr@hotmail.com>
2021-08-18 11:23:14 +0100

    completed data checks

--- app.js
+++ app.js
@@ -2,17 +2,21 @@ const express = require('express')
 const app = express()
 const port = 5000
 const cors = require('cors')
+const fileupload = require('express-fileupload')
 
 const cohortsRouter = require('./routes/cohorts.js')
 const studentsRouter = require('./routes/students.js')
+const fileRouter = require('./routes/fileUpload.js')
 
+app.use(fileupload())
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 app.use('/api/cohorts', cohortsRouter)
 app.use('/api/students', studentsRouter)
+app.use('/api/fileUpload', fileRouter)
 
 app.listen(port, () => {
-  "listening on port: ${port}"
+  console.log(`listening on port: ${port}`)
 })
