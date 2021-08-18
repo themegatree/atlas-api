bcd3553fa7376cebb9ee8c624b2a45f1b71f2e82
OllieVanD <Ollievdr@hotmail.com>
2021-08-18 09:21:38 +0100

    Added file checkout and some data checks

--- src/learningScoreCheck.js
+++
@@ -1,9 +0,0 @@
-function learningScoreCheck(confidenceScore,  overallScore, counter) {
-    if ( 0 > confidenceScore > 5){
-         errors.push(`The score on line ${counter} does not exist or is not within the limits for confidence score.`)
-    } 
-    if ( 0 > overallScore > 5){
-        errors.push(`The score on line ${counter} does not exist or is not within the limits for overall score.`)
-   } 
-}
-module.exports = learningScoreCheck
--- test/truncate-tables.js
+++
@@ -1,9 +0,0 @@
-const db = require('../models')
-
-const truncateTables = async () => {
-  console.log('truncating tables')
-  await db.Cohort.destroy({ truncate: true, cascade: true })
-  await db.Student.destroy({ truncate: true, cascade: true })
-}
-
-module.exports = truncateTables
