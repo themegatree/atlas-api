567c50cf5d7702b86696456ebe57835df7082a5b
Ben Ferenczy <bferenczy@gmail.com>
2021-08-19 16:36:52 +0100

    frontend sorting

--- routes/students.js
+++ routes/students.js
@@ -3,8 +3,7 @@ const router = express.Router()
 const { Student } = require('../models')
 
 router.get('/', async function (req, res) {
-  if (req.body.input === undefined) {req.body.input = 'firstName'}
-  const students = await Student.findAll({ include: { all: true }, order: [[req.body.input, "ASC"]]})
+  const students = await Student.findAll({ include: { all: true }})
   res.json({ students: students })
 })
 
