b9600b56514c51afbde04386aba68946629c8ff2
OllieVanD <Ollievdr@hotmail.com>
2021-08-20 12:44:17 +0100

    added Jasmine tests

---
+++ spec/Feature/file-upload.js/FileUploader.spec.js
@@ -0,0 +1,19 @@
+const FileUploader = require('../../../src/fileUpload.js')
+// const file = require('./csv-files/ModuleChallenges/passing.csv')
+// const reader = new FileReader()
+const fs = require('fs')
+
+describe("Feature tests for fileUploader" , () => {
+    xit("Testing overall SelfAssessment upload", ()=> {
+
+
+    })
+    xit("Testing overall ModuleChallenge upload", ()=> {
+
+        
+    })
+    xit("Testing overall Student upload", ()=> {
+
+        
+    })
+})
--- spec/unit/file-uploader/file-upload.spec.js
+++ spec/unit/file-uploader/file-upload.spec.js
@@ -1,27 +1,124 @@
 const FileUploader = require('../../../src/fileUpload.js')
-const file = require('./csv-files/ModuleChallenges/passing.csv')
+const { SelfAssessment, Student, ModuleChallenge } = require('../../../models')
 
-describe('Tests work for correct files', () => { 
-    it ('Works for Module Challenge', async () => {
-        const fileUpload = new FileUploader(file, 'moduleChallenge')
-        fileUpload.dataChecks()
-        const response =  await fileUpload.addToDatabase()
-        
-        expect(response).toEqual(["Updated the database successfully."])
+
+describe('Testing all functions in fileUploader class ,', () => { 
+    const moduleMockData = [{
+        StudentId: 1,
+        challengeName: 'RPS',
+        language: 'nodejs',
+        studentScore: 'extended',
+        coachScore: 'extended',
+        dueDate: '08/17/2021 15:45',
+        submissionDate: '08/17/2021 15:45'
+    }]
+    const learningMockData = [{
+        StudentId : 1,
+        confidenceScore : 3,
+        overallScore : 3,
+        studentReason : "No reason",
+        studentFeedback : "No Feedback",
+        dueDate : "08/17/21 14:35",
+        submissionDate : "08/17/21 14:35",
+    }]
+
+
+    describe('ModuleCheck functions ', () => {
+        it ('checking the projectCheck method works with no erros', async () => {
+            const fileUpload = new FileUploader("No file", 'moduleChallenge')
+            fileUpload.projectCheck('Chitter','javascript',6)
+            errors = fileUpload.errors
+            expect(errors).toEqual([])
+        })
+        it ('checking the projectCheck method works with errors', async () => {
+            const fileUpload = new FileUploader("No file", 'moduleChallenge')
+            fileUpload.projectCheck('Chitter','C#',6)
+            errors = fileUpload.errors
+            expect(errors).toEqual(['You have entered an incorrect language on line 6'])
+        })
+        it ('checking the scoreCheck method works with no errors', async () => {
+            const fileUpload = new FileUploader("No file", 'moduleChallenge')
+            fileUpload.scoreCheck('incomplete','incomplete',6)
+            errors = fileUpload.errors
+            expect(errors).toEqual([])
+        })
+        it ('checking the scoreCheck method works with errors', async () => {
+            const fileUpload = new FileUploader("No file", 'moduleChallenge')
+            fileUpload.scoreCheck('incomplete','finished',6)
+            errors = fileUpload.errors
+            expect(errors).toEqual(['Coach Score: finished is invalid, on line 6'])
+        })
+    })
+    
+    describe('DateCheck', () => {
+        it ('shows no errors for values before current date', async () => {
+            const fileUpload = new FileUploader("No file", 'moduleChallenge')
+            fileUpload.dateCheck("08/17/2021 15:45", "08/17/2021 15:45", 5)        
+            expect(fileUpload.errors).toEqual([])
+        })
+        it ('shows errors for values after current date', async () => {
+            const fileUpload = new FileUploader("No file", 'moduleChallenge')
+            fileUpload.dateCheck("08/17/2098 15:45", "08/17/2099 15:45", 5)        
+            expect(fileUpload.errors).toEqual(['Due date: Sun Aug 17 2098 15:45:00 GMT+0100 (British Summer Time) is invalid, on line 5','Submission date: Mon Aug 17 2099 15:45:00 GMT+0100 (British Summer Time) is invalid, on line 5'])
+        })
     })
 
-    it ('Works for Self Assessment', async() => {
-        const fileUpload = new FileUploader(file, 'selfAssessment')
-        fileUpload.dataChecks()
-        const response =  await fileUpload.addToDatabase()
-        expect(response).toEqual(["Updated the database successfully."])
+    describe('LearningScoreCheck', () => {
+        it ('shows no errors for values between 1 and 5', async () => {
+            const fileUpload = new FileUploader("No file", 'moduleChallenge')
+            fileUpload.learningScoreCheck(1, 5, 5)        
+            expect(fileUpload.errors).toEqual([])
+        })
+        it ('shows errors for values over 5', async () => {
+            const fileUpload = new FileUploader("No file", 'moduleChallenge')
+            fileUpload.learningScoreCheck(9, 1000, 5)        
+            expect(fileUpload.errors).toEqual(['The score: 9 on line 5 does not exist or is not within the limits for confidence score.','The score: 1000 on line 5 does not exist or is not within the limits for overall score.'])
+        })
+        it ('shows errors for values less than 1', async () => {
+            const fileUpload = new FileUploader("No file", 'moduleChallenge')
+            fileUpload.learningScoreCheck(0, -1000, 5)        
+            expect(fileUpload.errors).toEqual(['The score: 0 on line 5 does not exist or is not within the limits for confidence score.','The score: -1000 on line 5 does not exist or is not within the limits for overall score.'])
+        })
     })
-    it ("check async ", async () => {
-        const fileUpload = new FileUploader(file, 'selfAssessment')
-        fileUpload.dataChecks()
-        const response =  await fileUpload.addToDatabase()
-        expect(response).toEqual(["Updated the database successfully."])
+    
+    describe('Learning data check ', () =>{ 
 
+        it("Works with no errors", async  () => {
+            const fileUpload = new FileUploader("No file", 'selfAssessment')
+            spyOn(fileUpload, 'findAllStudents').and.returnValue([{id: 1}, {id: 2}])
+            fileUpload.data = learningMockData
+            await fileUpload.learningDataCheck()  
+
+            expect(fileUpload.errors).toEqual([])      
+        })
+        it("Works with errors", async () => {
+            const fileUpload = new FileUploader("No file", 'selfAssessment')
+            spyOn(fileUpload, 'findAllStudents').and.returnValue([{id: 1}, {id: 2}])
+            learningMockData[0].confidenceScore = 20
+            fileUpload.data = learningMockData
+            await fileUpload.learningDataCheck()  
+            expect(fileUpload.errors).toEqual(['The score: 20 on line 1 does not exist or is not within the limits for confidence score.'])      
+        })
     })
-    it ("check that it fails", )
-})
+    describe('Module data check ',  () => { 
+        it("Works with no errors", async () => {
+            const fileUpload = new FileUploader("No file", 'moduleChallenge')
+            spyOn(fileUpload, 'findAllStudents').and.returnValue([{id: 1}, {id: 2}])
+            fileUpload.data = moduleMockData
+            await fileUpload.moduleDataCheck()  
+            
+            expect(fileUpload.errors).toEqual([])
+        })
+
+        it("Works with errors", async () => {
+            const fileUpload = new FileUploader("No file", 'moduleChallenge')
+            spyOn(fileUpload, 'findAllStudents').and.returnValue([{id: 1}, {id: 2}])
+            moduleMockData[0].language = "C#"
+            fileUpload.data = moduleMockData
+            await fileUpload.moduleDataCheck()  
+            
+            expect(fileUpload.errors).toEqual(['You have entered an incorrect language on line 1'])
+        })
+    })
+
+});
