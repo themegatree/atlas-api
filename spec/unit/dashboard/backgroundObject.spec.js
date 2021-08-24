4a67304bae1eb6aa32ec7cb806631469a6b88a1d
Colin <cln.gardiner@outlook.com>
2021-08-24 12:15:55 +0100

    created background dashboard array
    Co-authored-by b7rch4 jamesbirchall4@btinternet.com

---
+++ spec/unit/dashboard/backgroundObject.spec.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+const truncateTables = require('../../../test/ReportGroupTests/truncate-tables') 
+const createCohorts = require('../../../test/ReportGroupTests/create-cohorts')
+const createStudents = require('../../../test/ReportGroupTests/create-students')
+const backgroundRatio = require('../../../src/dashboard/backgroundRatio');
+describe('background ratio test', () => {
+  let backgroundArr
+  beforeEach( async () => { 
+    await truncateTables()
+    await createCohorts()
+    await createStudents()
+    backgroundArr = await backgroundRatio();
+   })
+  it('calculates background ratio from background data', async () => {
+    expect(backgroundArr[0].percentage).toEqual('20.00');
+    expect(backgroundArr[1].percentage).toEqual('40.00');
+  });
+});
