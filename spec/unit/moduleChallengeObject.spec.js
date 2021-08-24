cc485a6a16ec61453b505644ccbaa1c692a88889
James Birchall <james.birchall4@btinternet.com>
2021-08-24 16:32:22 +0100

    Fixed an issue

--- spec/unit/moduleChallengeObject.spec.js
+++ spec/unit/moduleChallengeObject.spec.js
@@ -4,11 +4,9 @@ const createCohorts = require('../../test/ReportGroupTests/create-cohorts')
 const createStudents = require('../../test/ReportGroupTests/create-students')
 const createModuleChallenges = require('../../test/ReportGroupTests/create-module-challenges')
 const challengeRatio = require('../../src/reports/challengeRatio');
-
 describe('background ratio test', () => {
   let cohortId
   let challengeArr
-
   beforeEach( async () => { 
     await truncateTables()
     await createCohorts()
@@ -17,7 +15,6 @@ describe('background ratio test', () => {
     cohortId = 1;
     challengeArr = await challengeRatio(cohortId);
    })
-   
   it('calculates background ratio from background data', async () => {
     expect(challengeArr[1].type).toBe("Chitter")
   })
