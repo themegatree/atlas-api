98a70725223e6e1cfbe786574c76cb9d73f7a5ac
OllieVanD <Ollievdr@hotmail.com>
2021-08-26 14:07:44 +0100

    Added student upload, some Cypress tests and refactored some code

---
+++ src/fileUpload/dateCheck.js
@@ -0,0 +1,29 @@
+<<<<<<< HEAD:src/dateCheck.js
+module.exports = function dateCheck (dueDate, submissionDate, counter) {
+  const errors = [];
+  const currentDate = Date.now();
+  const newDueDate = new Date(dueDate);
+  const newSubmissionDate = new Date(submissionDate);
+  if (newDueDate > currentDate) {
+    errors.push(`Due date: ${dueDate} is invalid, on line ${counter}`);
+  }
+  if (newSubmissionDate > currentDate) {
+    errors.push(`Submission date: ${submissionDate} is invalid, on line ${counter}`);
+  }
+  return errors;
+};
+=======
+module.exports = function dateCheck (dueDate, submissionDate, counter) {
+  const errors = []
+  const currentDate = Date.now()
+  const newDueDate = new Date(dueDate)
+  const newSubmissionDate = new Date(submissionDate)
+  if (newDueDate > currentDate) {
+    errors.push(`Due date: ${dueDate} is invalid, on line ${counter}`)
+  }
+  if (newSubmissionDate > currentDate) {
+    errors.push(`Submission date: ${submissionDate} is invalid, on line ${counter}`)
+  }
+  return errors
+}
+>>>>>>> 3766703 (Added student upload, some Cypress tests and refactored some code):src/fileUpload/dateCheck.js
---
+++ src/fileUpload/utility.js
@@ -0,0 +1,4 @@
+module.exports = function checkArraysAreEqual (arr1, arr2) {
+    if (arr1.length !== arr2.length) return false
+    return arr1.every((el, index) => el === arr2[index])
+}
