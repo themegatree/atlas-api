7d4e27a64097639619b28d27eb9ff1daf0978c12
Ben Ferenczy <bferenczy@gmail.com>
2021-08-31 16:57:27 +0100

    student test

--- test/StudentGroupTests/create-student.js
+++ test/StudentGroupTests/create-student.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD:test/create-student.js
-const { Student } = require("../models");
-=======
 const { Student } = require("../../models");
->>>>>>> f4ef1d5 (linted student tests):test/StudentGroupTests/create-student.js
 
 const createStudent = async () => {
   console.log("creating students");
@@ -10,46 +6,30 @@ const createStudent = async () => {
     firstName: "Zara",
     lastName: "Marshall",
     githubUsername: "zaramgit",
-<<<<<<< HEAD:test/create-student.js
-    email: "zaram@email.com"
-=======
     email: "zaram@email.com",
     gender: "Female",
     background: "Black"
->>>>>>> f4ef1d5 (linted student tests):test/StudentGroupTests/create-student.js
   }, {
     firstName: "Jeff",
     lastName: "Jefferson",
     githubUsername: "jjgit",
-<<<<<<< HEAD:test/create-student.js
-    email: "jeff@email.com"
-=======
     email: "jeff@email.com",
     gender: "Male",
     background: "White"
->>>>>>> f4ef1d5 (linted student tests):test/StudentGroupTests/create-student.js
   }, {
     firstName: "Alice",
     lastName: "Williams",
     githubUsername: "alicewilliamsgit",
-<<<<<<< HEAD:test/create-student.js
-    email: "awilliams@email.com"
-=======
     email: "awilliams@email.com",
     gender: "Female",
     background: "Black"
->>>>>>> f4ef1d5 (linted student tests):test/StudentGroupTests/create-student.js
   }, {
     firstName: "Clive",
     lastName: "Smith",
     githubUsername: "csmithgit",
-<<<<<<< HEAD:test/create-student.js
-    email: "clivesmith@email.com"
-=======
     email: "clivesmith@email.com",
     gender: "Female",
     background: "Black"
->>>>>>> f4ef1d5 (linted student tests):test/StudentGroupTests/create-student.js
   }]);
 };
 
